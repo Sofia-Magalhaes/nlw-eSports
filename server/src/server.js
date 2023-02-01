2d34ab45fb8e6c8d1a1494d2bb70f2eeb809d1ca
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-01-31 22:22:59 -0300

    arquivo server.js certinho

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,6 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 //ECMAScript Modules
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
+import express from 'express';
+const app = express();
 app.get('/ads', (request, response) => {
     return response.send([
         { id: 1, name: 'Anúncio 1' },
