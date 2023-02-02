263984f73ddfa9cc0686ca44244ac57f8a1f77f3
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 17:26:02 -0300

    background, logo e frase

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 // JSX: Javascript + XML(HTML) -> escrever HTML diretamente do JS
 import "./styles/main.css";
 
+import logoImg from './assests/logo-nlw-esports.svg'
+
 function App() {
   return (
-  <div className="w-8 h-8 bg-violet-500"></div>
+  <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <img src={logoImg} alt="Logo" />
+
+    <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-nlw-gradient bg-clip-text text-transparent"> duo</span> está aqui.</h1>
+  </div>
   )
 }
 
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,15 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {  
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+      },
+    },
   },
   plugins: [],
 }
