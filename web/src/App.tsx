11ba6df604a1dfe16043175a7fd36ddcfdd7f7ff
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 18:15:53 -0300

    publicar anúncio e pacote phosphor
    npm i phosphor-react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -16,54 +16,74 @@ function App() {
         </span>{" "}
         está aqui.
       </h1>
+      {/* Div dos jogos */}
       <div className="grid grid-cols-6 gap-6 mt-16">
         <a href="" className="relative  rounded-lg overflow-hidden">
           <img src="/game-1.png" alt="" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">League of Legends</strong>
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
-        </a>  
+        </a>
         <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-2.png" alt="" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font-bold text-white block">Dota 2</strong>
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
-        </a>  
+        </a>
         <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-3.png" alt="" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Counter Strike</strong>
+            <strong className="font-bold text-white block">
+              Counter Strike
+            </strong>
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
-        </a>  
+        </a>
         <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-4.png" alt="" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font-bold text-white block">Apex Legends</strong>
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
-        </a>  
+        </a>
         <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-5.png" alt="" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font-bold text-white block">Fortnite</strong>
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
-        </a>  
+        </a>
         <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-6.png" alt="" />
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <strong className="font-bold text-white block">
+              World of Warcraft
+            </strong>
             <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
-        </a>  
+        </a>
+      </div>
+      {/* Fim da div dos jogos */}
+      {/* Box */}
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>{" "}
+          </div>
 
-             
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">Publicar anúncio</button>
+        </div>
       </div>
     </div>
-    
   );
 }
 
