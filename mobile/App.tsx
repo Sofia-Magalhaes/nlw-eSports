eca1e9cf25e1768e53ca6eabd63ac713bc77689a
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 19:03:26 -0300

    status bar
    melhorando visibilidade do usuario

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,15 @@
+import { StatusBar } from "react-native";
+
 import { Background } from "./src/components/Background";
 
 export default function App() {
   return (
-    <Background></Background>
+    <Background>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+    </Background>
   );
 }
