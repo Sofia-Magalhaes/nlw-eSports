97b69182ede8e3b8f3273de1ee3a7491e2dafced
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 19:42:36 -0300

    logo
    mostrando logo

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, Image } from "react-native";
 
-import { styles } from './styles';
+import loogImg from "../../assets/logo-nlw-esports.png";
+
+import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-
+      <Image source={loogImg} style={styles.logo} />
     </View>
   );
 }
