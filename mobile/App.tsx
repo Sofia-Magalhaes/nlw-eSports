d7ae96c312f99e4735036f20e33f6d5f65d05726
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 19:24:36 -0300

    colocando fonte no mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,22 @@
 import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 import { Background } from "./src/components/Background";
 
 export default function App() {
+  useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+  
   return (
     <Background>
       <StatusBar
