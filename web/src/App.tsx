a7d5ff6c214d4bcd222e2c30661177b10900e85e
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 17:43:20 -0300

    nome do jogo - conteiner

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -17,23 +17,47 @@ function App() {
         está aqui.
       </h1>
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="">
+        <a href="" className="relative  rounded-lg overflow-hidden">
           <img src="/game-1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>  
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>  
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>  
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>  
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>  
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
         </a>  
 
              
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       backgroundImage: {  
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
