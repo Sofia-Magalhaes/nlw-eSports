1fcb4913744ed5a04e5e173aac2920c8888c2158
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 19:57:03 -0300

    titulo e subtitulo + começo cardGame

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
 import { View, Image } from "react-native";
 
 import loogImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+
 
 import { styles } from "./styles";
 
@@ -9,6 +10,8 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={loogImg} style={styles.logo} />
+
+      <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..."/>
     </View>
   );
 }
