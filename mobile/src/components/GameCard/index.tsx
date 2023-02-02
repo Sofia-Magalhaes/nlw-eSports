c28ba7f3130f3668a8b5d9ce65863ee10ec050b3
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 20:17:49 -0300

    card game aparecendo

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,11 +1,27 @@
-import { View } from 'react-native';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ImageBackground,
+  ImageSourcePropType,
+} from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-export function GameCard() {
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: Props) {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground style={styles.cover} source={data.cover} />
+    </TouchableOpacity>
   );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import { View, Image } from "react-native";
 
 import loogImg from "../../assets/logo-nlw-esports.png";
+
+import { GameCard } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
 
-
+import { GAMES } from "../../utils/games";
 import { styles } from "./styles";
 
 export function Home() {
@@ -11,7 +13,11 @@ export function Home() {
     <View style={styles.container}>
       <Image source={loogImg} style={styles.logo} />
 
-      <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..."/>
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+      <GameCard data={GAMES[0]}/>
     </View>
   );
 }
