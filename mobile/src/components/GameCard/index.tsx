74cbbf0d3858a88c5de9a9bdd046c9f9ca7ee9ad
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-02 20:22:19 -0300

    gradient do cardGame

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -3,9 +3,13 @@ import {
   TouchableOpacityProps,
   ImageBackground,
   ImageSourcePropType,
+  Text,
 } from "react-native";
 
+import { LinearGradient } from "expo-linear-gradient";
+
 import { styles } from "./styles";
+import { THEME } from "../../theme";
 
 export interface GameCardProps {
   id: string;
@@ -21,7 +25,12 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground style={styles.cover} source={data.cover} />
+      <ImageBackground style={styles.cover} source={data.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
     </TouchableOpacity>
   );
 }
