b88ed41049b62e810f36e6ac2cc693f2f7257e2c
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-03 16:21:41 -0300

    criando outras rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,55 @@
 //ECMAScript Modules
-import express from 'express'
+
+// HTTP methods / API RESTful / HTTP Codes (mostra se a resposta que se recebe é valida ou não)
+
+// status que começa com: 2 (sucesso), 3 (redirecionamento), 4 (erro por código errado), 5 (erro inesperado)
+
+// GET(buscar informação), 
+// POST(criar algo),
+// PUT(editar uma entidade),
+// PATCH(editar uma informação especifica de uma entidade),
+// DELETE(remover alguama entidade)
+
+// Tipos de parâmetros
+/**
+ * Query: Vem atraves do ponto de interrogação (precisa persistir estado) usado para filtros, ordenação,
+ *  paginação, coisas que não são sensiveis(senha). São sempre nomeados. Parametro de URL
+ * Route: É também um parametro de URL, não são nomeados. Reconhecido só de olhar
+ * Body: Usado quando se vai mandar varios dados para uma única requisição (geralmente para envio de formulário). 
+ * Não fica na URL, fica escondido.
+ */
+
+import express, { request, response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+
   return response.send([
-    {id: 1, name:'Anúncio 1'},
-    {id: 2, name:'Anúncio 2'},
-    {id: 3, name:'Anúncio 3'},
-    {id: 4, name:'Anúncio 4'},
-    {id: 5, name:'Anúncio 5'},
+    { id: 1, name: 'Anúncio 1' },
+    { id: 2, name: 'Anúncio 2' },
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' },
+  ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+
+  return response.send([
   ])
 })
 
