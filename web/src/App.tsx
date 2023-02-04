d6fec434f0963c17d3f29533999fb620d8fd2c0e
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-04 13:25:03 -0300

    axios funcionando

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,6 +10,7 @@ import "./styles/main.css";
 import logoImg from "./assests/logo-nlw-esports.svg";
 
 import { CreateAdModal } from "./components/CreateAdModal";
+import axios from "axios";
 
 interface Game {
   id: string;
@@ -24,10 +25,8 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch(" http://localhost:3333/games")
-      .then((response) => response.json())
-      .then((data) => {
-        setGames(data);
+    axios(" http://localhost:3333/games").then((response) => {
+        setGames(response.data);
       });
   }, []);
 
