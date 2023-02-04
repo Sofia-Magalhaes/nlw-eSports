72e8298d6bba77d971fd94cbff09067eb576afee
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-04 00:45:03 -0300

    buscando games do back-end

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from "react";
 import { View, Image, FlatList } from "react-native";
 
 import loogImg from "../../assets/logo-nlw-esports.png";
 
-import { GameCard } from "../../components/GameCard";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
 
-import { GAMES } from "../../utils/games";
 import { styles } from "./styles";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://192.168.0.99:3333/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={loogImg} style={styles.logo} />
@@ -19,9 +27,9 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
-        renderItem={({item}) => <GameCard data={item} />}
+        renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
         horizontal
         contentContainerStyle={styles.contentList}
