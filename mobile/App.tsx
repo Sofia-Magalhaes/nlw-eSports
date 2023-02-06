2944c4bc56a18eb48626e70ff3c9c6094df4b400
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-02-05 23:33:56 -0300

    mobile concluido
    notificação sem back-end

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, { useRef, useEffect } from "react";
 import { StatusBar } from "react-native";
 import {
   useFonts,
@@ -7,21 +7,47 @@ import {
   Inter_700Bold,
   Inter_900Black,
 } from "@expo-google-fonts/inter";
+import { Subscription } from "expo-modules-core";
 
 import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
 import { Background } from "./src/components/Background";
 
-import './src/services/notificationConfigs';
-import { getPushNotificationToken } from "./src/services/getPushNotificationToken"; 
+import "./src/services/notificationConfigs";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({  
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_900Black
+    Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  });
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification =>{
+      console.log(notification);
+    });
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response =>{
+      console.log(response);
+    });
+
+    return() => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  },[]);
 
   return (
     <Background>
@@ -30,7 +56,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes/> : <Loading/> }
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
